a874c3bb3b602ddbcd7827ccac6062b907bb4aea
Nathaniel Rudavsky-Brody <nathaniel.rudavsky@tenforce.com>
2017-03-30 16:02:28 +0200

    more

--- import.scm
+++ import.scm
@@ -2,15 +2,32 @@
 
 (load "rdf.scm")
 
-(define datafile (with-input-from-file "../../data/cleaneddata1-470111.csv" read-string))
+(define (load path)
+  (with-input-from-file path read-string))
 
-;;(define datafile (with-input-from-file "../../data/short.csv" read-string))
+(define datafile (with-input-from-file "../../data/cleaneddata1-470111.csv" read-string))
 
 (define parse (csv-parser #\,))
 
-(define data
+;; Slow!
+
+(define (load-data)
   (cdr
    (map csv-record->list (parse datafile))))
+
+;; Lazy - dosn't work (EOF) and what is gained?
+
+(define (lazy-load-data path)
+  (let ((port (open-input-file path)))
+    (generator->lseq
+     (lambda ()
+       (csv-record->list
+	(join
+	 (parse
+	  (read-line
+	   port))))))))
+
+;; (define lazy-data (load-data  "../../data/cleaneddata1-470111.csv"))
 
 (define-namespace es "http://tenforce.com/eurostat")
 
@@ -18,37 +35,35 @@
 
 (define-namespace terms "http://tenforce.com/terms")
 
+(define (triple datum)
+  (let ((isba (third datum))
+	(esba (fifth datum))
+	(esba-desc-terms (string-split (sixth datum)))
+	(gtin (seventh datum))
+	(gtin-desc-terms (string-split (eighth datum))))
+    `(
+      ,(make-triple (es gtin) #:a (stats "GTIN"))
+      ,(make-triple (es isba) #:a (stats "ISBA"))
+      ,(make-triple (es gtin) (stats "isba") (es isba))
+      ,@(join
+	 (map (lambda (term)
+	       (list (make-triple (es gtin) (stats "hasTerm") (terms term))
+		     (make-triple (terms term) #:a (stats "Term"))))
+	      (join (list esba-desc-terms
+			  gtin-desc-terms)))))))
+
 (define (triples data)
   (join
-   (map (lambda (datum)
-	  (let ((isba (third datum))
-		(esba (fifth datum))
-		(esba-desc-terms (string-split (sixth datum)))
-		(gtin (seventh datum))
-		(gtin-desc-terms (string-split (eighth datum))))
-	    `(
-	      ,(make-triple (es gtin) (stats "isba") (es isba))
-	      ,@(map (lambda (term) (make-triple (es gtin) (stats "hasTerm") (terms term)))
-		     (join (list esba-desc-terms
-				 gtin-desc-terms))))))
-	data)))
+   (map triple data)))
 
-(define (run m n)
+(define (run-triple datum)
   (sparql/update (*sparql-endpoint*)
-		 (insert-triples (take (drop (triples data) m)
-				       n)
+		 (insert-triples (triple datum)
 				 (*default-graph*))))
 
-(define retailer-descriptions-words
-  (map string-split retailer-descriptions))
+(define (run data)
+  (for-each run-triple data))
 
-(define word-bank
-  (delete-duplicates
-   (join retailer-descriptions-words)))
-
-(define feature-lists
-  (map (lambda (desc)
-	 (map (lambda (word)
-		(and (memq word desc) #t))
-	      word-bank))
-       retailer-descriptions-words))
+;; (define word-bank
+;;   (delete-duplicates
+;;    (join retailer-descriptions-words)))
